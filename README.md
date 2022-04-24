283f733e6cb79c4a6041d2adf07c0d7ddfb63437
boedegoat <bhremada.fka@gmail.com>
2022-04-24 14:09:04 +0700

    setup tailwind colors, add readme

---
+++ README.md
@@ -0,0 +1,50 @@
+# Project Name
+
+<!-- screenshots here -->
+
+!()[./screenshot.png]
+
+A challenge from [frontendmentor.io](https://www.frontendmentor.io/)
+
+## Overview
+
+### Links
+
+- [Live Site](https://your-live-site-url.com)
+- [Solution](https://www.frontendmentor.io/profile/boedegoat)
+
+### The challenge
+
+- See the size of the elements adjust based on their device's screen size
+- Perform mathematical operations like addition, subtraction, multiplication, and division
+- Adjust the color theme based on their preference
+- **Bonus**: Have their initial theme preference checked using `prefers-color-scheme` and have any additional changes saved in the browser
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Tailwind CSS
+- React
+- Next JS
+
+### What I learned
+
+- What do you think about this project ?
+- What have you learned from this project ?
+- Is there any issue while doing this project ?
+- How you solve those issue ?
+
+### Useful Resources
+
+1. [Figma](https://www.figma.com/) - Paste your project design mockups to check the size, colors, etc.
+2. [Vite](https://vitejs.dev/) - Next Generation Frontend Tooling
+
+## Acknowledgments
+
+Thank you very much to everyone who gave me feedback on my solution. It greatly assists me in improving my frontend development skills.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,61 @@
 module.exports = {
   content: ['./src/**/*.{ts,tsx,js,jsx}', './index.html'],
   theme: {
+    backgroundColor: {
+      theme1: {
+        main: 'hsl(222, 26%, 31%)',
+        keypad: 'hsl(223, 31%, 20%)',
+        screen: 'hsl(224, 36%, 15%)',
+      },
+      theme2: {
+        main: 'hsl(0, 0%, 90%)',
+        keypad: 'hsl(0, 5%, 81%)',
+        screen: 'hsl(0, 0%, 93%)',
+      },
+      theme3: {
+        main: 'hsl(268, 75%, 9%)',
+        screen: 'hsl(268, 71%, 12%)',
+      },
+    },
+    colors: {
+      theme1: {
+        keys: {
+          blue: 'hsl(225, 21%, 49%)',
+          'blue-shadow': 'hsl(224, 28%, 35%)',
+          red: 'hsl(6, 63%, 50%)',
+          'red-shadow': 'hsl(6, 70%, 34%)',
+          light: 'hsl(30, 25%, 89%)',
+          'light-shadow': 'hsl(28, 16%, 65%)',
+        },
+        text: 'hsl(221, 14%, 31%)',
+      },
+      theme2: {
+        keys: {
+          cyan: 'hsl(185, 42%, 37%)',
+          'cyan-shadow': 'hsl(185, 58%, 25%)',
+          orange: 'hsl(25, 98%, 40%)',
+          'orange-shadow': 'hsl(25, 99%, 27%)',
+          light: 'hsl(45, 7%, 89%)',
+          'light-shadow': 'hsl(35, 11%, 61%)',
+        },
+        text: 'hsl(60, 10%, 19%)',
+      },
+      theme3: {
+        keys: {
+          violet: 'hsl(281, 89%, 26%)',
+          'violet-shadow': 'hsl(285, 91%, 52%)',
+          cyan: 'hsl(176, 100%, 44%)',
+          'cyan-shadow': 'hsl(177, 92%, 70%)',
+          dark: 'hsl(268, 47%, 21%)',
+          'dark-shadow': 'hsl(290, 70%, 36%)',
+        },
+        text: {
+          yellow: 'hsl(52, 100%, 62%)',
+          dark: 'hsl(198, 20%, 13%)',
+        },
+      },
+      white: 'hsl(0, 0, 100%)',
+    },
     extend: {},
   },
   plugins: [],
